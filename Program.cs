bb1284ccaad53c9eb5ee180cb98186c0ec2ae95a
Guilliam Oberholster <guilliamoberholster@gmail.com>
2022-05-24 09:35:00 +1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Conditionals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = 10;
+
+            if (hour > 0 && hour < 12)
+            {
+                Console.WriteLine("Its Morning!");
+            }
+            else if (hour >= 12 && hour < 18)
+            {
+                Console.WriteLine("Its Afternoon!");
+            }
+            else
+            { 
+                Console.WriteLine("Its Evening");
+            }
+        }
+    }
+}
